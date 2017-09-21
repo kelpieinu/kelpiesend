dd7c98dd798f7970a615662616e88573e8296862
sashamihaha <sashamihaha@gmail.com>
2017-09-21 03:11:43 +0300

    receive countdown

--- src/components/ReceiveTab/VerifiedProxy/ConfirmForm.jsx
+++ src/components/ReceiveTab/VerifiedProxy/ConfirmForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Countdown from "react-countdown-clock";
 import web3Api from "../../../apis/web3-common-api";
 import PhoneForm from './PhoneForm';
 import TxProgress from './ReceiveTxProgress';
@@ -9,59 +9,84 @@ export default class ConfirmForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-		    smsCode: "",
-		    isFetching: false,
-		    step: 0,
-		    txId: "",
-		    txAmount: 0
+			phone: this.props.phone,
+			code: this.props.code,
+			phoneCode: this.props.phoneCode,
+			smsCode: "",
+			isFetching: false,
+			step: 0,
+			txId: "",
+			txAmount: 0,
+			countdown: true
 		};
 	}
 
 
 	submit() {
-	    const component = this;
-	    console.log(this.state);
-	    this.setState({ isFetching: true, step: 1 });
-	    eth2phoneApi.verifyPhoneAndWithdraw(this.props.phoneCode,
-						this.props.phone,
-						this.props.code,
-						this.state.smsCode,
-						this.props.to)
-		.then(function (result) {
-		    console.log({ result });
-		    // tx is pending (not mined yet)
-		    component.setState({ step: 2, txId: result.pendingTxHash, txAmount : result.amount});
-		    return web3Api.getTransactionReceiptMined(result.pendingTxHash);
-		}).then((txReceipt) => {
-		    console.log("Tx mined!");
-		    component.setState({ isFetching: false, step: 3 });
-		}).catch(function (err) {
-		    console.log({ err });
-		    component.setState({
-			error: (err.message || err.errorMessage || "Server error!"),
-			isFetching: false,
-			step: 0
-		    });
-		});
+		const component = this;
+		console.log(this.state);
+		this.setState({ isFetching: true, step: 1 });
+		eth2phoneApi.verifyPhoneAndWithdraw(this.props.phoneCode,
+			this.props.phone,
+			this.props.code,
+			this.state.smsCode,
+			this.props.to)
+			.then(function (result) {
+				console.log({ result });
+				// tx is pending (not mined yet)
+				component.setState({ step: 2, txId: result.pendingTxHash, txAmount: result.amount });
+				return web3Api.getTransactionReceiptMined(result.pendingTxHash);
+			}).then((txReceipt) => {
+				console.log("Tx mined!");
+				component.setState({ isFetching: false, step: 3 });
+			}).catch(function (err) {
+				console.log({ err });
+				component.setState({
+					error: (err.message || err.errorMessage || "Server error!"),
+					isFetching: false,
+					step: 0
+				});
+			});
 	}
 
-
+	resend() {
+		const component = this;
+		eth2phoneApi.sendSmsToPhone(component.state.phoneCode, component.state.phone, component.state.code).then(function (result) {
+			if (!result.success) {
+				throw new Error((result.errorMessage || "Server error"));
+			}
+		}).catch(function (err) {
+			console.log({ err });
+		});
+		component.setState({ countdown: true })
+	}
 
 	render() {
-	    const component = this;
-	    return (
-		<div>
-		  <div>
-		    {this.state.step > 0 ? <div className="modal-body" style={{ marginBottom: "50px" }}>
-			  <TxProgress step={this.state.step} txId={this.state.txId} address={this.props.to} txAmount={this.state.txAmount}/></div> : <div>
-			  <input className="form-control" type="text" onChange={(event) => this.setState({ smsCode: event.target.value })} placeholder="Enter SMS code you've received" />
-		    </div>}
-		  </div>
-		  <div>
-		    {this.state.step === 0 ? <a className="btn btn-md btn-accent" onClick={() => component.submit()}>Send</a> : ""}
-		    <span style={{ color: "red" }} > {component.state.error}</span>
-		  </div>
-		</div>
-	    );
+		const component = this;
+		return (
+			<form>
+				{this.state.step > 0 ? <div className="modal-body" style={{ marginBottom: "50px" }}>
+					<TxProgress step={this.state.step} txId={this.state.txId} address={this.props.to} txAmount={this.state.txAmount} /></div> :
+					<div className="input-container">
+						<div style={{ width: "80%", float: "left" }} >
+							<input className="form-control" type="text" style={{ marginTop: "5px" }} onChange={(event) => this.setState({ smsCode: event.target.value })} placeholder="Enter SMS code you've received" />
+						</div>
+						<div style={{ width: "17%", float: "right" }}>
+							{component.state.countdown === true ?
+								<Countdown seconds={60}
+									color="#f6a821"
+									alpha={0.9}
+									size={45}
+									onComplete={() => { component.setState({ countdown: false }) }} /> : <a className="btn btn-md btn-accent" style={{ marginTop: "5px" }} onClick={() => component.resend()}>Resend</a>}</div>
+					</div>
+				}
+				<br />
+				<br />
+				<div className="col-md-12" style={{ marginLeft: "-10px" }}>
+					{this.state.step === 0 ? <a className="btn btn-md btn-accent" onClick={() => component.submit()}>Confirm</a> : ""}
+					<span style={{ color: "red" }} > {component.state.error}</span>
+				</div>
+			</form>
+		);
 	}
 }
--- src/components/ReceiveTab/VerifiedProxy/PhoneForm.jsx
+++ src/components/ReceiveTab/VerifiedProxy/PhoneForm.jsx
@@ -8,12 +8,12 @@ import eth2phoneApi from "../../../apis/eth2phone-api";
 
 export default class ReceivePhoneTab extends Component {
     constructor(props) {
-	let phoneParams = {};
+        let phoneParams = {};
         super(props);
-	if (this.props.phone) {
-	    phoneParams = this._checkPhone(this.props.phone);
-	}
-	
+        if (this.props.phone) {
+            phoneParams = this._checkPhone(this.props.phone);
+        }
+
         this.state = {
             phone: this.props.phone,
             phoneCode: phoneParams.phoneCode,
@@ -41,15 +41,16 @@ export default class ReceivePhoneTab extends Component {
                 isFetching: false
             });
         });
+
     }
 
-    _checkPhone(phone)  {
+    _checkPhone(phone) {
         const phoneIsValid = isValidNumber(phone);
         const formatter = new asYouType();
         formatter.input(phone);
-        const phoneParams =  { phoneCode: formatter.country_phone_code, phone, phoneIsValid };
-	console.log({phoneParams});
-	return phoneParams;
+        const phoneParams = { phoneCode: formatter.country_phone_code, phone, phoneIsValid };
+        console.log({ phoneParams });
+        return phoneParams;
     }
 
 
@@ -57,38 +58,38 @@ export default class ReceivePhoneTab extends Component {
         const component = this;
         return (
             <form>
-              <div>
-                <label>
-                  Your phone number
+                <div>
+                    <label>
+                        Your phone number
                 </label>
-              </div>
-              <div className="input-container">
-		<div style={{ width: "90%", float: "left" }}>
-		  <Phone value={component.state.phone} onChange={phone => component.setState(component._checkPhone(phone))} />
-		</div>
-		<div style={{ width: "10%", float: "right" }}>{this.state.phoneIsValid ?  <div className="green-tick" >&#10003;</div> : ""}</div></div>
+                </div>
+                <div className="input-container">
+                    <div style={{ width: "90%", float: "left" }}>
+                        <Phone value={component.state.phone} onChange={phone => component.setState(component._checkPhone(phone))} />
+                    </div>
+                    <div style={{ width: "10%", float: "right" }}>{this.state.phoneIsValid ? <div className="green-tick" >&#10003;</div> : ""}</div></div>
 
-              
-              <br />
-              <div>
-                <label>
-                  Verification code
+
+                <br />
+                <div>
+                    <label>
+                        Verification code
                 </label>
-              </div>
-              <div className="input-container">
-		<div style={{ width: "90%", float: "left" }}>
-                  <input className="form-control" type="text" value={component.state.code} onChange={(event) => this.setState({ code: event.target.value })} />
-                </div><div style={{ width: "10%", float: "right" }}>{this.state.code.length === 8 ? <div className="green-tick" >&#10003;</div> : ""}</div></div>
-              <br />
-              <div>
-                {this.state.isFetching ? <div className="loader-spin"></div> : ""}
+                </div>
+                <div className="input-container">
+                    <div style={{ width: "90%", float: "left" }}>
+                        <input className="form-control" type="text" value={component.state.code} onChange={(event) => this.setState({ code: event.target.value })} />
+                    </div><div style={{ width: "10%", float: "right" }}>{this.state.code.length === 8 ? <div className="green-tick" >&#10003;</div> : ""}</div></div>
+                <br />
+                <div>
+                    {this.state.isFetching ? <div className="loader-spin"></div> : ""}
 
-                <a className="btn btn-md btn-default" onClick={() => this.props.goBack()}> Go Back </a>
-                <a className="btn btn-md btn-accent btn-margin-left" onClick={() => component.submit()}> Submit </a>
+                    <a className="btn btn-md btn-default" onClick={() => this.props.goBack()}> Go Back </a>
+                    <a className="btn btn-md btn-accent btn-margin-left" onClick={() => component.submit()}> Submit </a>
 
-                <span style={{ color: "red" }} > {component.state.error}</span>
+                    <span style={{ color: "red" }} > {component.state.error}</span>
 
-              </div>
+                </div>
             </form>
         );
     }
